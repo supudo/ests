b57a2628312f17e289824cca31d2f175c54c1de5
Sergey Petrov <supudo@supudo.net>
2015-04-17 14:41:19 +0300

    rev

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,10 @@ class DashboardController < ApplicationController
 
   def index 
     add_breadcrumb I18n.t('breadcrumbs.dashboard'), :dashboard_path
+    @estimates = Estimate.order("created_date DESC").paginate(page: 1, :per_page => 10)
+    @projects = Project.order("created_date DESC").paginate(page: 1, :per_page => 10)
+    @clients = Client.order("created_date DESC").paginate(page: 1, :per_page => 10)
+    @users = User.order("created_date DESC").paginate(page: 1, :per_page => 10)
   end
 
 end
