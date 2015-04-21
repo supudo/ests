e8e96691723ad6b3e3afa8002f962695f6a5612b
Sergey Petrov <supudo@supudo.net>
2015-04-21 22:10:14 +0300

    rev

---
+++ TODO.markdown
@@ -0,0 +1,4 @@
+http://goodybag.github.io/bootstrap-notify/
+http://www.codingdrama.com/bootstrap-markdown/
+
+http://jschr.github.io/bootstrap-modal/
--- app/controllers/technologies_controller.rb
+++ app/controllers/technologies_controller.rb
@@ -74,6 +74,6 @@ class TechnologiesController < ApplicationController
   private
 
     def technology_params
-      params.require(:technology).permit(:title)
+      params.require(:technology).permit(:title, :style)
     end
 end
