753843091adcaafebfefab1564474d813afd69ab
Sergey Petrov <supudo@supudo.net>
2015-04-21 19:01:50 +0300

    rev

--- app/controllers/estimatessection_controller.rb
+++ app/controllers/estimatessection_controller.rb
@@ -2,6 +2,13 @@ class EstimatessectionController < ApplicationController
   before_action :signed_in_user
   helper_method :sort_column, :sort_direction
 
+  def show
+    EstimatesLine.where(:estimates_sections_id => esection_technology_params[:sid]).update_all(:technology_id => esection_technology_params[:tid])
+    respond_to do |format|
+      format.js
+    end
+  end
+
   def create
     if EstimatesSection.exists?(:estimates_sheet_id => estimates_section_params[:estimates_sheet_id], :title => estimates_section_params[:title])
       redirect_to :new_estimatessection
@@ -26,6 +33,10 @@ class EstimatessectionController < ApplicationController
 
     def estimates_section_params
       params.require(:estimate_section).permit(:estimate_id, :estimates_sheet_id, :title)
+    end
+
+    def esection_technology_params
+      params.require(:esection_technology).permit(:sid, :tid)
     end
 
     def sort_column
