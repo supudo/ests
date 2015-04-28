4a168587257bbe624e92edd96e485de78665accf
Sergey Petrov <supudo@supudo.net>
2015-04-28 16:31:57 +0300

    rev

--- app/controllers/estimate_exporter_controller.rb
+++ app/controllers/estimate_exporter_controller.rb
@@ -15,32 +15,34 @@ class EstimateExporterController < ApplicationController
     f_workbook = estimate_file.workbook
     f_sheet = f_workbook.add_worksheet(:name => t('export_general_sheet'))
 
+    cs_black = f_workbook.styles.add_style :bg_color => "4C", :fg_color => "FF", :alignment => { :horizontal=> :left }, :b => true
+    cs_black_right = f_workbook.styles.add_style :bg_color => "4C", :fg_color => "FF", :alignment => { :horizontal=> :right }, :b => true
+    cs_grey = f_workbook.styles.add_style :bg_color => "CC", :fg_color => "00", :alignment => { :horizontal=> :left }, :b => true
+    cs_bold = f_workbook.styles.add_style :b => true
+
     #General
-    f_sheet.add_row [t('client'), file_client]
-    f_sheet.add_row [t('project'), file_project]
-    f_sheet.add_row [t('export_date'), Time.now.strftime("%d/%m/%Y")]
-    f_sheet.add_row [t('export_version'), '1']
+    f_sheet.add_row [t('client'), file_client], :style => [cs_bold, nil]
+    f_sheet.add_row [t('project'), file_project], :style => [cs_bold, nil]
+    f_sheet.add_row [t('export_date'), Time.now.strftime("%d/%m/%Y")], :style => [cs_bold, nil]
+    f_sheet.add_row [t('export_version'), 1], :style => [cs_bold, nil]
     f_sheet.add_row []
 
     # Assumptions
     c = 0
     assumptions.each do |f_assumption|
       if c == 0
-        f_sheet.add_row [t('assumptions'), f_assumption.title]
+        f_sheet.add_row [t('assumptions'), f_assumption.title], :style => [cs_bold, nil]
       end
       f_sheet.add_row ['', f_assumption.title]
       c += 1
     end
     f_sheet.add_row []
-
-    cs_black = nil
-    cs_grey = nil
-    f_workbook.styles do |f_style|
-      cs_black = f_style.add_style :bg_color => "4C", :fg_color => "FF", :alignment => { :horizontal=> :left, :b => true }
-      cs_grey = f_style.add_style :bg_color => "CC", :fg_color => "00", :alignment => { :horizontal=> :left, :b => true }
-    end
       
-    f_sheet.add_row ['', t('estimate_task'), t('estimate_hours_min'), t('estimate_hours_max'), t('estimate_rate'), t('estimate_cost_min'), t('estimate_cost_max')]
+    f_sheet.add_row ['', t('estimate_task'), t('estimate_hours_min'), t('estimate_hours_max'), t('estimate_rate'), t('estimate_cost_min'), t('estimate_cost_max')], :style => [nil, cs_black, cs_black, cs_black, cs_black, cs_black, cs_black]
+    f_sheet.add_row [], :style => [nil, cs_black, cs_black, cs_black, cs_black, cs_black, cs_black]
+
+    estimate_total_hours_min = 0
+    estimate_total_hours_max = 0
     # Sheets
     sheets.each do |sheet|
 
@@ -50,6 +52,8 @@ class EstimateExporterController < ApplicationController
       sections = EstimatesSection.where(:estimates_sheet_id => sheet.id).order("id ASC")
       sections.each do |f_section|
         f_sheet.add_row ['', f_section.title, f_section.total_min_hours, f_section.total_max_hours, '', '', ''], :style => [nil, cs_grey, cs_grey, cs_grey, cs_grey, cs_grey, cs_grey]
+        estimate_total_hours_min += f_section.total_min_hours
+        estimate_total_hours_max += f_section.total_max_hours
 
         # Lines
         lines = EstimatesLine.where(:estimates_sections_id => f_section.id).order("line_number ASC")
@@ -60,7 +64,8 @@ class EstimateExporterController < ApplicationController
       end
 
     end
-
+    f_sheet.add_row ['', t('total_caps'), estimate_total_hours_min, estimate_total_hours_max, '', '', ''], :style => [nil, cs_black_right, cs_black, cs_black, cs_black, cs_black, cs_black]
+    
     # Options
     estimate_file.use_shared_strings = true
     
