b253b5dfe382f034763ad09bfe4e83a3ca2471ba
Sergey Petrov <supudo@supudo.net>
2015-04-28 17:57:32 +0300

    rev

--- app/controllers/estimate_exporter_controller.rb
+++ app/controllers/estimate_exporter_controller.rb
@@ -22,23 +22,39 @@ class EstimateExporterController < ApplicationController
     cs_bold = f_workbook.styles.add_style :b => true, :alignment => { :horizontal=> :left }
     cs_normal = f_workbook.styles.add_style :alignment => { :horizontal=> :left }
 
+    # Styles - Borders
+    defaults =  { :style => :medium, :color => "000000" }
+    borders = Hash.new do |hash, key|
+      hash[key] = f_workbook.styles.add_style :border => defaults.merge( { :edges => key.to_s.split('_').map(&:to_sym) } )
+    end
+
+    # Counters
+    counter_row = 0
+
     #General
     f_sheet.add_row [t('client'), file_client], :style => [cs_bold, nil]
     f_sheet.add_row [t('project'), file_project], :style => [cs_bold, nil]
     f_sheet.add_row [t('export_date'), Time.now.strftime("%d/%m/%Y")], :style => [cs_bold, nil]
     f_sheet.add_row [t('export_version'), 1], :style => [cs_bold, cs_normal]
     f_sheet.add_row []
+    counter_row += 5
 
     # Assumptions
     c = 0
     assumptions.each do |f_assumption|
       if c == 0
-        f_sheet.add_row [t('assumptions'), f_assumption.title], :style => [cs_bold, nil]
+        f_sheet.merge_cells("B" + counter_row.to_s + ":G" + counter_row.to_s)
+        f_sheet.add_row [t('assumptions'), f_assumption.title, '', '', '', '', '', ''], :style => [cs_bold, borders[:top_left], borders[:top], borders[:top], borders[:top], borders[:top], borders[:top], borders[:left]]
+      else
+        f_sheet.merge_cells("B" + counter_row.to_s + ":G" + counter_row.to_s)
+        f_sheet.add_row ['', f_assumption.title, '', '', '', '', '', ''], :style => [borders[:right], 0, 0, 0, 0, 0, 0, borders[:left]]
       end
-      f_sheet.add_row ['', f_assumption.title]
+
       c += 1
+      counter_row += 1
     end
-    f_sheet.add_row []
+    f_sheet.merge_cells("B" + counter_row.to_s + ":G" + counter_row.to_s)
+    f_sheet.add_row ['', '', '', '', '', '', ''], :style => [0, borders[:top], borders[:top], borders[:top], borders[:top], borders[:top], borders[:top], borders[:top]]
 
     # Header
     f_sheet.add_row ['', t('estimate_task'), t('estimate_hours_min'), t('estimate_hours_max'), t('estimate_rate'), t('estimate_cost_min'), t('estimate_cost_max')], :style => [nil, cs_black, cs_black, cs_black, cs_black, cs_black, cs_black]
