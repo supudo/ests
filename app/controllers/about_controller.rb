608f76e3a05b20db7b764a35290456e34b344f00
Sergey Petrov <supudo@supudo.net>
2015-05-11 11:21:19 +0300

    rev

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,6 +1,7 @@
 class AboutController < ApplicationController
 
   def index
+    add_breadcrumb I18n.t('about'), about_index_path
   end
 
 end
--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,6 +1,7 @@
 class HelpController < ApplicationController
 
   def index
+    add_breadcrumb I18n.t('help'), help_index_path
   end
 
 end
