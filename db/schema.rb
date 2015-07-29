19f1247afe6cfc50999ef3b05b12539e0dfe8777
Sergey Petrov <sergey@stanga.net>
2015-07-29 14:49:43 +0300

    rev

--- db/schema.rb
+++ db/schema.rb
@@ -161,9 +161,9 @@ ActiveRecord::Schema.define(version: 0) do
   add_index "estimates_sheets", ["estimate_id"], name: "estimate_id", using: :btree
 
   create_table "permissions", force: :cascade do |t|
-    t.integer "parent_id",   limit: 4,     default: 0, null: false
-    t.string  "code",        limit: 255,               null: false
-    t.text    "description", limit: 65535,             null: false
+    t.integer "parent_id",   limit: 4,     null: false
+    t.string  "code",        limit: 255,   null: false
+    t.text    "description", limit: 65535, null: false
   end
 
   create_table "positions", force: :cascade do |t|
@@ -265,18 +265,27 @@ ActiveRecord::Schema.define(version: 0) do
   add_index "technologies", ["title"], name: "title", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",           limit: 150,             null: false
-    t.string   "password_digest", limit: 255,             null: false
-    t.string   "first_name",      limit: 150,             null: false
-    t.string   "last_name",       limit: 150,             null: false
-    t.string   "remember_token",  limit: 255,             null: false
-    t.integer  "technology_id",   limit: 4,               null: false
-    t.integer  "position_id",     limit: 4,               null: false
-    t.integer  "client_id",       limit: 4,               null: false
-    t.integer  "is_pdm",          limit: 1,   default: 0, null: false
-    t.integer  "is_am",           limit: 1,   default: 0, null: false
-    t.datetime "created_date",                            null: false
-    t.datetime "last_modified",                           null: false
+    t.string   "email",                  limit: 150,              null: false
+    t.string   "password_digest",        limit: 255,              null: false
+    t.string   "first_name",             limit: 150,              null: false
+    t.string   "last_name",              limit: 150,              null: false
+    t.string   "remember_token",         limit: 255,              null: false
+    t.integer  "technology_id",          limit: 4,                null: false
+    t.integer  "position_id",            limit: 4,                null: false
+    t.integer  "client_id",              limit: 4,                null: false
+    t.integer  "is_pdm",                 limit: 1,   default: 0,  null: false
+    t.integer  "is_am",                  limit: 1,   default: 0,  null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_date",                                    null: false
+    t.datetime "last_modified",                                   null: false
   end
 
   add_index "users", ["client_id"], name: "client_id", using: :btree
